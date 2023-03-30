4ca68127fb2447f1b12c820f5c8ea8d9b6c735ba
misia <damaske.magdalena@wp.pl>
2023-03-30 13:24:27 +0200

    added language table

--- models.py
+++ models.py
@@ -1,3 +1,17 @@
+from datetime import datetime
+from sqlalchemy import Integer, Column, String, DateTime, ForeignKey, Text, Table
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
+class Language(Base):
+    __tablename__ = "languages"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(20), nullable = False, unique = True)
+
+    def __repr__(self):
+        return f'Language ({self.name})'
+
